2db5733819ec61bdd9a18bc9e52daebd5a52ef99
Srinjay-GIT <das.srinjay.sphs@gmail.com>
2026-03-13 21:42:30 +0530

    1. Project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Srinjay Das",
+    author_email="das.srinjay.sphs@gmail.com",
+    packages=find_packages()
+)
