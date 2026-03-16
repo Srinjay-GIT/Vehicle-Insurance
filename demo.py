b68f60d6a423cb5f8a12c877d46768dda335a9be
Srinjay-GIT <das.srinjay.sphs@gmail.com>
2026-03-16 22:43:07 +0530

    3. Data Ingestion pipeline complete

--- demo.py
+++ demo.py
@@ -3,13 +3,27 @@
 #logging.debug("This is a debug message.")
 
 # # below code is to check the exception config
-from src.logger import logging
-from src.exception import MyException
-import sys
+#from src.logger import logging
+#from src.exception import MyException
+#import sys
 
-try:
-     a = 1+'Z'
-except Exception as e:
+#try:
+#     a = 1+'Z'
+#except Exception as e:
 
-     logging.info(e)
-     raise MyException(e, sys) from e
+#     logging.info(e)
+#     raise MyException(e, sys) from e
+
+'''
+    Workflow:
+    Constant
+    Config_Entity
+    Artifact_entity
+    Component
+    Pipeline
+    app.py / demo.py
+'''
+
+from src.pipline.training_pipeline import TrainPipeline
+pipeline = TrainPipeline()
+pipeline.run_pipeline()
